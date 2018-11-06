9185179151b62813a7a5377efc7f7fea02eb79fb
walkmiao <372815340@qq.com>
2018-11-06 12:02:43 +0800

    优化处理

--- demo1.py
+++ demo1.py
@@ -5,12 +5,21 @@
 # @File    : demo1.py
 import xlrd
 from xlutils.copy import copy
-from config import input_excel_path, output_excel_path, input_path_list
+# from config import input_excel_path, output_excel_path, input_path_list
 from threading import Thread
 from logger import logger
 import random
 import os
 import re
+import configparser
+
+def get_conf(filepath='./config.ini'):
+    conf = configparser.ConfigParser()
+    conf.read(filepath, encoding='utf-8')
+    path_list = conf.get('PATH', 'path_list')
+    path_list = path_list.split(',')
+    output_excel_path = conf.get('PATH', 'output_path')
+    return path_list, output_excel_path
 
 
 def get_table(path_list):
@@ -21,11 +30,12 @@ def get_table(path_list):
         table_dict[path_name] = sheet_dict
     return table_dict
 
-def get_sheet(path=input_excel_path):
+
+def get_sheet(path):
     wb = xlrd.open_workbook(path)
     sheets = wb.sheets()
     sheets_name = wb.sheet_names()
-    if path!=output_excel_path:
+    if len(sheets)>3:
         sheet_dict={}
         sheets = sheets[1:]
         sheets_name = sheets_name[1:]
@@ -36,7 +46,7 @@ def get_sheet(path=input_excel_path):
         return sheets[0]
 
 
-def get_wb_copy(path=output_excel_path):
+def get_wb_copy(path):
     wb = xlrd.open_workbook(path)
     wb_copy = copy(wb)
     return wb_copy
@@ -70,56 +80,107 @@ class ExcelHandle(Thread):
         return sheet
 
     def complex_re(self, warn_value):
+        high1='high1'
+        low1='low1'
+        high2='high2'
+        low2='low2'
+        result={}
         if '/' in warn_value:
-            result = warn_value.split('/')
+            r = warn_value.split('/')
+            low_value, high_value = r
+            if self.what_type(low_value) >self.what_type(high_value):
+                low_value,high_value=high_value,low_value
+            result[high1]=high_value
+            result[low1]=low_value
             return result
 
-        if '>' in warn_value:
-            if '<' in warn_value:
-                result = re.findall(r'.*<(-?\d+\.?\d?).*>(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*<(-?\d+\.?\d?).*>(-?\d+\.?\d?).*', warn_value) else None
+        if '>' in warn_value and '<' in warn_value:
+            if warn_value.count('>')==1:
+                if warn_value.count('<')==1:
+                    high_value = re.findall(r'.*?>(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    low_value = re.findall(r'.*?<(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    result[high1] = high_value
+                    result[low1] = low_value
+                else:
+                    high_value = re.findall(r'.*?>(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    low_value1,low_value2 = re.findall(r'.*?<(-?\d+\.?\d{0,4}).*<(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    result[high1] = high_value
+                    result[low2] = low_value2
+                    result[low1] = low_value1
             else:
-                result = re.findall(r'.*>(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*>(-?\d+\.?\d?).*', warn_value) else None
+                if warn_value.count('<') == 1:
+                    r= re.findall(r'.*?>(-?\d+\.?\d{0,4}).*>(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    high_value1, high_value2 = r
+                    low_value = re.findall(r'.*?<(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    result[high1] = high_value1
+                    result[high2] = high_value2
+                    result[low1] = low_value
+                else:
+                    high_value1, high_value2 = re.findall(r'.*?>(-?\d+\.?\d{0,4}).*>(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    low_value1, low_value2 = re.findall(r'.*?<(-?\d+\.?\d{0,4}).*<(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                    result[high1] = high_value1
+                    result[high2] = high_value2
+                    result[low1] = low_value1
+                    result[low2] = low_value2
             return result
 
-        if'<' in warn_value:
-            if '>' in warn_value:
-                result = re.findall(r'.*<(-?\d+\.?\d?).*>(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*<(-?\d+\.?\d?).*>(-?\d+\.?\d?).*', warn_value) else None
+        if '>' in warn_value and '<' not in warn_value:
+            if warn_value.count('>')==1:
+                high_value = re.findall(r'.*?>(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                result[high1] = high_value
             else:
-                result = re.findall(r'.*<(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*<(-?\d+\.?\d?).*', warn_value) else None
+                high_value1, high_value2 = re.findall(r'.*?>(-?\d+\.?\d{0,4}).*>(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                result[high1] = high_value1
+                result[high2] = high_value2
+            return result
+        if '<' in warn_value and '>' not in warn_value:
+            if warn_value.count('<')==1:
+                low_value = re.findall(r'.*?<(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                result[low1] = low_value
+            else:
+                low_value1, low_value2 = re.findall(r'.*?<(-?\d+\.?\d{0,4}).*<(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                result[low1] = low_value1
+                result[low2] = low_value2
             return result
 
         if '>=' in warn_value:
             if '<=' in warn_value:
-                result = re.findall(r'.*>=(-?\d+\.?\d?).*<=(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*>=(-?\d+\.?\d?).*<=(-?\d+\.?\d?).*', warn_value) else None
+                high_value1, low_value1 = re.findall(r'.*?>=(-?\d+\.?\d{0,4}).*<=(-?\d+\.?\d{0,4}).*', warn_value)[0] \
+                    if re.findall(r'.*?>=(-?\d+\.?\d{0,4}).*<=(-?\d+\.?\d{0,4}).*', warn_value) else None
+                result[high1] = high_value1
+                result[low1] = low_value1
             else:
-                result = re.findall(r'.*>=(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*>=(-?\d+\.?\d?).*', warn_value) else None
+                high_value1 = re.findall(r'.*?>=(-?\d+\.?\d{0,4}).*', warn_value)[0] \
+                    if re.findall(r'.*?>=(-?\d+\.?\d{0,4}).*', warn_value) else None
+                result[high1] = high_value1
             return result
 
         if '≥' in warn_value:
             if '≤' in warn_value:
-                result = re.findall(r'.*≥(-?\d+\.?\d?).*≤(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*≥(-?\d+\.?\d?).*≤(-?\d+\.?\d?).*', warn_value) else None
+                high_value1, low_value1 = re.findall(r'.*?≥(-?\d+\.?\d{0,4}).*≤(-?\d+\.?\d{0,4}).*', warn_value)[0]
+                result[high1] = high_value1
+                result[low1] = low_value1
             else:
-                result = re.findall(r'.*≥(-?\d+\.?\d?).*', warn_value)[0] \
-                    if re.findall(r'.*≥(-?\d+\.?\d?).*', warn_value) else None
+                high_value1 = re.findall(r'.*?≥(-?\d+\.?\d{0,4}).*', warn_value)[0] \
+                    if re.findall(r'.*?≥(-?\d+\.?\d{0,4}).*', warn_value) else None
+                result[high1] = high_value1
             return result
 
         if ',' in warn_value and '>':
-            result = warn_value.split(',')
+            low_value1, high_value1 = warn_value.split(',')
+            if self.what_type(low_value1) >self.what_type(high_value1):
+                low_value1,high_value1=high_value1,low_value1
+            result[high1] = high_value1
+            result[low1] = low_value1
             return result
         if '，' in warn_value:
-            result = warn_value.split('，')
+            low_value1, high_value1 = warn_value.split('，')
+            if self.what_type(low_value1) >self.what_type(high_value1):
+                low_value1,high_value1=high_value1,low_value1
+            result[high1] = high_value1
+            result[low1] = low_value1
             return result
         return warn_value
-
-
-
 
     def insert_to_excle(self):
         mrid_col_value = self.get_out_put_excel_info()[1:]
@@ -132,22 +193,26 @@ class ExcelHandle(Thread):
                     warn_value = str(self.input_sheet.cell_value(index_input, 6))
                     result = self.complex_re(warn_value)
                     try:
-                        if not isinstance(result, str) and result:
-                            if len(result) > 1:
-                                value_low, value_high = result
-                                if self.what_type(value_low) > self.what_type(value_high):
-                                    w_sheet.write(index_output, 10, self.what_type(value_high))
-                                    w_sheet.write(index_output, 9, self.what_type(value_low))
-                                else:
-                                    w_sheet.write(index_output, 10, self.what_type(value_low))
-                                    w_sheet.write(index_output, 9, self.what_type(value_high))
+                        if result:
+                            if isinstance(result, dict):
+                                w_sheet.write(index_output, 8, 1)
+                                if 'high1'in result:
+                                    w_sheet.write(index_output, 9, self.what_type(result['high1']))
+                                if 'high2' in result:
+                                    w_sheet.write(index_output, 12, self.what_type(result['high2']))
+                                if 'low1' in result:
+                                    w_sheet.write(index_output, 10, self.what_type(result['low1']))
+                                if 'low2' in result:
+                                    w_sheet.write(index_output, 13, self.what_type(result['low1']))
                             else:
-                                w_sheet.write(index_output, 10, self.what_type(result))
+                                try:
+                                    w_sheet.write(index_output, 9, self.what_type(result))
+                                    w_sheet.write(index_output, 8, 1)
+                                except Exception:
+                                    w_sheet.write(index_output, 9, result)
+                                    w_sheet.write(index_output, 8, 1)
                         else:
-                            try:
-                                w_sheet.write(index_output, 10, self.what_type(result))
-                            except Exception:
-                                w_sheet.write(index_output, 10, result)
+                            pass
 
                     except Exception as e:
                         logger.info('[ERROR] CAUSE BY{},result={}'.format(e, result))
@@ -160,10 +225,11 @@ class ExcelHandle(Thread):
 
 
 def main():
-    tab_dict = get_table(input_path_list)
-    # sheet_dict = get_sheet()
+    path_list, output_excel_path = get_conf()
+    tab_dict = get_table(path_list)
+
     out_sheet = get_sheet(output_excel_path)
-    wb_copy = get_wb_copy()
+    wb_copy = get_wb_copy(output_excel_path)
     thread_list = []
     for tab_name, sheet_dict in tab_dict.items():
         for input_sheet, sheet_name in sheet_dict.items():
@@ -173,12 +239,15 @@ def main():
         t.start()
     for t in thread_list:
         t.join()
+    out_base_name = os.path.basename(output_excel_path) +'输出结果'
     wb_copy.save('./result.xlsx')
     try:
-        os.rename('./result.xlsx', './result.xls')
+        os.rename('./result.xlsx', './{}.xls'.format(out_base_name))
     except Exception:
         os.rename('./result.xlsx','./result'+ str(random.randint(1,10))+'.xls')
     logger.info('执行完毕')
 
+
 if __name__ == '__main__':
-        main()
+    main()
+    # print(get_conf())
